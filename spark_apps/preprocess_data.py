addc4b40a0fef58563440a4d484ce0339f6f3a8a
silviaperelli <silviaperelli02@gmail.com>
2025-05-22 17:33:11 +0200

    preprocess data

--- spark_apps/preprocess_data.py
+++ spark_apps/preprocess_data.py
@@ -3,36 +3,6 @@ import time
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, year, month, dayofmonth, hour, to_timestamp, lit, when, upper, avg
 
-def normalize_country_code(country_col_original, zone_id_col):
-    """
-    Normalizzazione del nome del paese in un codice ISO a 2 lettere (maiuscolo).
-    """
-    country_code = when(upper(country_col_original) == "ITALY", lit("IT")) \
-        .when(upper(country_col_original) == "SWEDEN", lit("SE")) \
-        .when(upper(country_col_original) == "GERMANY", lit("DE")) \
-        .when(upper(country_col_original) == "FRANCE", lit("FR")) \
-        .when(upper(country_col_original) == "SPAIN", lit("ES")) \
-        .when(upper(country_col_original) == "AUSTRIA", lit("AT")) \
-        .when(upper(country_col_original) == "BELGIUM", lit("BE")) \
-        .when(upper(country_col_original) == "FINLAND", lit("FI")) \
-        .when(upper(country_col_original) == "UNITED KINGDOM", lit("GB")) \
-        .when(upper(country_col_original) == "IRELAND", lit("IE")) \
-        .when(upper(country_col_original) == "NORWAY", lit("NO")) \
-        .when(upper(country_col_original) == "POLAND", lit("PL")) \
-        .when(upper(country_col_original) == "CZECH REPUBLIC", lit("CZ")) \
-        .when(upper(country_col_original) == "SLOVENIA", lit("SI")) \
-        .when(upper(country_col_original) == "SWITZERLAND", lit("CH")) \
-        .when(upper(country_col_original) == "UNITED STATES", lit("US")) \
-        .when(upper(country_col_original) == "UNITED ARAB EMIRATES", lit("AE")) \
-        .when(upper(country_col_original) == "CHINA", lit("CN")) \
-        .when(upper(country_col_original) == "INDIA", lit("IN")) \
-        .otherwise(
-        when(zone_id_col.isNotNull() & zone_id_col.rlike("^[A-Z]{2}"), upper(zone_id_col.substr(1, 2))) # Gestione più robusta per zone_id
-        .otherwise(lit("UNKNOWN"))
-    )
-    return country_code
-
-
 if __name__ == "__main__":
     start_time = time.time()
     spark = SparkSession.builder \
@@ -57,6 +27,7 @@ if __name__ == "__main__":
         columns_to_drop = [
             "Carbon_intensity_gCO_eq_kWh__Life_cycle_",
             "Renewable_energy_percentage__RE__",
+            "Zone_name",
             "Data_source",
             "Data_estimated",
             "Data_estimation_method"
@@ -70,7 +41,6 @@ if __name__ == "__main__":
         else:
             print("Nessuna delle colonne specificate per la rimozione è presente nel DataFrame.")
 
-
         print("Schema dati grezzi (dopo la rimozione colonne):")
         df_raw.printSchema()
         df_raw.show(5, truncate=False)
@@ -82,33 +52,12 @@ if __name__ == "__main__":
 
     df_intermediate = df_raw \
         .withColumn("datetime", to_timestamp(col("Datetime__UTC_"))) \
-        .withColumn("carbon_intensity_val", col("Carbon_intensity_gCO_eq_kWh__direct_").cast("double")) \
-        .withColumn("carbon_free_percentage_val", col("Carbon_free_energy_percentage__CFE__").cast("double")) \
-        .withColumn("country_code", normalize_country_code(col("Country"), col("Zone_id"))) \
-        .filter(col("country_code") != "UNKNOWN") \
-        .select(
-            "datetime",
-            "country_code",
-            "carbon_intensity_val",
-            "carbon_free_percentage_val"
-        )
+        .withColumn("carbon_intensity", col("Carbon_intensity_gCO_eq_kWh__direct_").cast("double")) \
+        .withColumn("carbon_free_percentage", col("Carbon_free_energy_percentage__CFE__").cast("double")) \
+        .withColumn("country_code", col("Zone_id")) \
+        .withColumn("country", col("Country")) \
 
-    print("Schema dati dopo trasformazioni iniziali e filtro (pronto per l'aggregazione per timestamp):")
-    df_intermediate.printSchema()
-    df_intermediate.show(5, truncate=False)
-
-    df_aggregated_by_timestamp = df_intermediate \
-        .groupBy("datetime", "country_code") \
-        .agg(
-            avg("carbon_intensity_val").alias("carbon_intensity"),
-            avg("carbon_free_percentage_val").alias("carbon_free_percentage")
-        )
-
-    print("Schema dati dopo l'aggregazione per paese e timestamp:")
-    df_aggregated_by_timestamp.printSchema()
-    df_aggregated_by_timestamp.show(5, truncate=False)
-
-    df_final = df_aggregated_by_timestamp \
+    df_final = df_intermediate \
         .withColumn("year", year(col("datetime"))) \
         .withColumn("month", month(col("datetime"))) \
         .withColumn("day", dayofmonth(col("datetime"))) \
@@ -116,7 +65,7 @@ if __name__ == "__main__":
         .select(
             "datetime",
             "country_code",
-            col("country_code").alias("country"),
+            "country",
             "carbon_intensity",
             "carbon_free_percentage",
             "year",
@@ -136,7 +85,7 @@ if __name__ == "__main__":
     output_path_processed = "hdfs://namenode:8020/spark_data/spark"
     try:
         df_final.write \
-            .partitionBy("country") \
+            .partitionBy("country_code") \
             .mode("overwrite") \
             .parquet(output_path_processed)
         print(f"Dati aggregati per paese e timestamp, partizionati per paese, salvati in {output_path_processed}")
--- spark_apps/query.py
+++ spark_apps/query.py
@@ -8,7 +8,8 @@ spark = SparkSession.builder \
 spark.sparkContext.setLogLevel("WARN")
 
 # 2. Leggi il file Parquet da HDFS (modifica il path se necessario)
-df = spark.read.parquet("hdfs://namenode:8020/spark_data/spark/country=IT/part-00000-c7d351b0-f2e7-45a8-8ecc-f0cb1a5441ad.c000.snappy.parquet")
+#df = spark.read.parquet("hdfs://namenode:8020/spark_data/spark/country=IT/part-00000-c7d351b0-f2e7-45a8-8ecc-f0cb1a5441ad.c000.snappy.parquet")
+df = spark.read.parquet("hdfs://namenode:8020/nifi_data/electricity_maps/IT/2021/IT_2021_hourly.parquet")
 
 # 3. Registra il DataFrame come vista temporanea SQL
 df.createOrReplaceTempView("dati")
